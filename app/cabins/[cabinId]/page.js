4a36972d99d75a636f585287e62bd46e6827faff
Abdul Wahid Jahangir <abdulwahidjahangir12@outlook.com>
2024-06-18 19:20:14 +0200

    updating single cabin page

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,6 +1,5 @@
 import Cabin from "@/app/_components/Cabin";
 import Reservation from "@/app/_components/Reservation";
-import ReservationReminder from "@/app/_components/ReservationReminder";
 import Spinner from "@/app/_components/Spinner";
 
 import { getCabin, getCabins } from "@/app/_lib/data-service";
@@ -11,17 +10,6 @@ export async function generateMetadata({ params }) {
 
   return { title: `Cabin ${name} / ` };
 }
-
-export async function generateStaticParams() {
-  const cabins = await getCabins();
-
-  const ids = cabins.map((cabin) => {
-    cabinId: String(cabin.id);
-  });
-
-  return ids;
-}
-
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
 
